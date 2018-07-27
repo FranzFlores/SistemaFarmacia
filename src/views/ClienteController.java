4017137e2e0ce736d2acd917d9516192916b1799
luis1112 <40868489+luis1112@users.noreply.github.com>
2018-07-27 18:17:25 -0500

    complemento vistas
    (vistas) cliente, información, proveedores, usuario

---
+++ src/views/ClienteController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views;
+
+import java.awt.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rodrigo
+ */
+public class ClienteController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+   /* @FXML
+    private TextField Nombre;
+   
+     @FXML
+    private TextField Apellido;
+     
+      @FXML
+    private TextField Cedula;
+      
+       @FXML
+    private TextField Direccion;
+       
+        @FXML
+    private TextField Telefano;
+        
+         @FXML
+    private TextField Celular;
+    */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
